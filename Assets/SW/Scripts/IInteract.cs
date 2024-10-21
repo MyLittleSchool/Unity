8222b6ff599f6f7579ba2fbe71257190c95556ee
aa13245 <163653941+aa13245@users.noreply.github.com>
2024-10-21 21:03:00 +0900

    상호작용 기능
    구조 최적화 / 툴팁 UI 버그 수정

--- Assets/SW/Scripts/IInteract.cs
+++
@@ -1,7 +0,0 @@
-public interface IInteract
-{
-    public void Interact();
-    public string GetInfo();
-    public void HighlightOn();
-    public void HighlightOff();
-}
--- Assets/SW/Scripts/PlayerInteracter.cs
+++ Assets/SW/Scripts/PlayerInteracter.cs
@@ -3,31 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SW
 {
     public class PlayerInteracter : MonoBehaviour
     {
-        private List<IInteract> inTrigged;
+        private List<Interactive> inTrigged;
         public GameObject ui;
         public TMP_Text uiText;
+        public RectTransform panel;
         void Start()
         {
-            inTrigged = new List<IInteract>();
+            inTrigged = new List<Interactive>();
         }
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            inTrigged.Add(collision.GetComponent<IInteract>());
-            ui.SetActive(true);
+            inTrigged.Add(collision.GetComponent<Interactive>());
             if (inTrigged.Count == 1)
             {
                 uiText.text = inTrigged.First().GetInfo();
                 inTrigged.First().HighlightOn();
+                ui.SetActive(true);
+                LayoutRebuilder.ForceRebuildLayoutImmediate(panel);
             }
         }
         private void OnTriggerExit2D(Collider2D collision)
         {
-            IInteract i = collision.GetComponent<IInteract>();
+            Interactive i = collision.GetComponent<Interactive>();
             i.HighlightOff();
             inTrigged.Remove(i);
             
@@ -35,9 +38,12 @@ namespace SW
             {
                 uiText.text = inTrigged.First().GetInfo();
                 inTrigged.First().HighlightOn();
+                ui.SetActive(true);
+                LayoutRebuilder.ForceRebuildLayoutImmediate(panel);
             }
             else
             {
+                uiText.text = "";
                 ui.SetActive(false);
             }
         }
