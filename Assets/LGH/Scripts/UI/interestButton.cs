da365824fabd02c49651b836500f454249098aca
LeeGyuhyeons <ghl7276@gmail.com>
2024-11-05 21:49:38 +0900

    회원가입 로그인 완성

--- Assets/LGH/Scripts/UI/interestButton.cs
+++ Assets/LGH/Scripts/UI/interestButton.cs
@@ -9,19 +9,14 @@ namespace GH
 
     public class interestButton : MonoBehaviour
     {
-        private TMP_Text interText;
         private TMP_Text buttonText;
 
-        private bool onSelect = false;
-        private Image image;
         private Button button;
 
         private LoginJoinUIManager loginJoinUIManager;
         void Start()
         {
-            interText = GameObject.Find("interest_Text").GetComponent<TMP_Text>();
             buttonText = GetComponentInChildren<TMP_Text>();
-            image = GetComponent<Image>();
             button = GetComponent<Button>();
             loginJoinUIManager = GameObject.Find("UIManager").GetComponentInChildren<LoginJoinUIManager>();
             button.onClick.AddListener(InterOnClick);
@@ -29,25 +24,12 @@ namespace GH
 
         void Update()
         {
-            if (onSelect)
-            {
-                image.color = new Color32(242, 136, 75, 255);
-            }
-            else
-            {
-                image.color = new Color32(242, 242, 242, 255);
-
-            }
+           
 
         }
         public void InterOnClick()
         {
-            onSelect = onSelect ? false : true;
-
-            if(onSelect)
-            {
-                //loginJoinUIManager.interestsQueue.Enqueue(buttonText.text);
-            }
+            loginJoinUIManager.InterestSlect(buttonText.text, GetComponent<Image>());
         }
     }
 
