b5e10aecd76de853ee3bd0a739046e344b017737
LeeGyuhyeons <ghl7276@gmail.com>
2024-11-14 23:40:18 +0900

    3차취합 중

--- Assets/LGH/Scripts/Scene/GoQuizSquar.cs
+++ Assets/LGH/Scripts/Scene/GoQuizSquar.cs
@@ -3,10 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntoQuizSquare : MonoBehaviour
+public class GoQuizSquar : MonoBehaviour
 {
-  
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player") && collision.gameObject.GetComponent<PhotonView>().IsMine)
