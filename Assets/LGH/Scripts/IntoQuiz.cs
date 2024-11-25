fe4d5b0816fd0c1b844c0fe155446d6b296ec6c9
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2024-11-25 17:43:32 +0900

    컴파일 오류 수정
    친구 따라가기
    기존: 퀴즈맵
    변경: 퀴즈 스퀘어

--- Assets/LGH/Scripts/IntoQuiz.cs
+++ Assets/LGH/Scripts/IntoQuiz.cs
@@ -20,7 +20,6 @@ public class IntoQuiz : MonoBehaviour
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player") && collision.gameObject.GetComponent<PhotonView>().IsMine)
         {
-            SceneMgr.instance.QuizIn();
             QuestManager.instance.QuestPatch(5);
             SceneUIManager.GetInstance().OnQuizCategoryPanel();
         }
