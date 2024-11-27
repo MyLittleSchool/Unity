a9f5e63d04cb5ec9c4bc309436ba43ca93596ca9
LeeGyuhyeons <ghl7276@gmail.com>
2024-11-27 19:58:05 +0900

    url변경

--- Assets/HMJ/Scripts/Inventory/ShopSystem.cs
+++ Assets/HMJ/Scripts/Inventory/ShopSystem.cs
@@ -4,8 +4,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
