315edb8aa591a27c8177d50d902509372ea4814e
aa13245 <wl031512@gmail.com>
2024-12-11 16:17:34 +0900

    학교방문NPC

---
+++ Assets/SW/Scripts/VisitOtherSchoolNPC.cs
@@ -0,0 +1,65 @@
+using MJ;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+namespace SW
+{
+    public class VisitOtherSchoolNPC : Interactive
+    {
+        public GameObject chatBalloon;
+        public TMP_Text chatBallonText;
+        public string text;
+        public int time = 5;
+        private bool chatEnable;
+        public bool ChatEnable
+        {
+            get { return chatEnable; }
+            set
+            {
+                chatEnable = value;
+                if (chatEnable)
+                {
+                    StopAllCoroutines();
+                    StartCoroutine(SetChatBallon());
+                }
+                else
+                {
+                    StopAllCoroutines();
+                    chatBalloon.SetActive(false);
+                }
+            }
+        }
+        public override void Interact()
+        {
+            SceneUIManager.GetInstance().OnVisitOtherSchoolPanel();
+        }
+        protected override void Start()
+        {
+            base.Start();
+            chatBallonText.text = text;
+        }
+        public override void HighlightOn()
+        {
+            base.HighlightOn();
+            ChatEnable = true;
+        }
+        public override void HighlightOff()
+        {
+            base.HighlightOff();
+            if (ChatEnable)
+            {
+                ChatEnable = false;
+            }
+        }
+        private void OnDestroy()
+        {
+            HighlightOff();
+        }
+        private IEnumerator SetChatBallon()
+        {
+            chatBalloon.SetActive(true);
+            yield return new WaitForSeconds(time);
+            chatBalloon.SetActive(false);
+        }
+    }
+}
